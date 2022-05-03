ab6a00c5e5d9eda69df545d7e63fe94e82e2c365
Matteo Gironda <60718820+matteogironda@users.noreply.github.com>
2022-05-02 18:59:23 -0700

    Add files via upload

---
+++ get_current_game.py
@@ -0,0 +1,34 @@
+import cassiopeia as cass
+from cassiopeia import Summoner
+
+def getAPI_key():
+    f = open("./api_key.txt", "r")
+    return f.read()
+
+def get_result(summoner):
+    
+    print("in get result")
+    cass.set_riot_api_key(getAPI_key())
+    last_match = summoner.match_history[0]
+    champion_id_to_name_mapping = {
+        champion.id: champion.name for champion in cass.get_champions(region='NA')
+    }
+    champ_played_id = last_match.participants[summoner].champion.id
+    champ_played_name = champion_id_to_name_mapping[champ_played_id]
+    win = last_match.participants[summoner].stats.win
+
+    return win, champ_played_name
+
+def throw_flame(summoner_name):
+    
+    print("in throw flame")
+    summoner = Summoner(name=summoner_name, region="NA")
+    win, champ = get_result(summoner)
+
+    if win:
+        pass
+    else:
+        print("\n" + f"{summoner_name}" + ', you fuckin scrub, you lost your last game as ' + f"{champ}" + '.')
+
+if __name__ == "__main__":
+    throw_flame("YodelYaddle")
